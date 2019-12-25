c952345828987dbd8ac28a1d0e16e7921d46de87
raver119 <raver119@gmail.com>
2019-12-25 10:35:05 +0300

    restore timeout back again
    Signed-off-by: raver119 <raver119@gmail.com>

--- nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/BaseNd4jTest.java
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/BaseNd4jTest.java
@@ -52,8 +52,8 @@ public abstract class BaseNd4jTest {
     @Rule
     public TestName testName = new TestName();
 
-    //@Rule
-    //public Timeout timeout = Timeout.seconds(30);
+    @Rule
+    public Timeout timeout = Timeout.seconds(30);
 
     protected long startTime;
     protected int threadCountBefore;
